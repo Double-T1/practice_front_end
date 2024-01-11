4a4cec39d738ef0668f6af608c02a1f906089d8c
chen weichen <newsforcwc@gmail.com>
2024-01-11 19:04:00 +0800

    popup menu for changing daily goal added

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -9,12 +9,12 @@ class Content extends React.Component {
 	}
 
 	render() {
-		const {route, name, todayMins, onSubmitInput, 
+		const {route, name, todayMins, onSubmitInput, setShowPopUp,
 			onRouteChange, id, email, loadUser, setLoading,
 			unloadUser, totalMins, totalDays, streaks, dailyGoal} = this.props;
 		return (
 			route === "progress" ? 
-			( <Progress name={name} todayMins={todayMins} 
+			( <Progress name={name} todayMins={todayMins} setShowPopUp={setShowPopUp}
 				onSubmitInput={onSubmitInput} totalMins={totalMins} 
 				totalDays={totalDays} streaks={streaks} dailyGoal={dailyGoal}/> 
 			) :
